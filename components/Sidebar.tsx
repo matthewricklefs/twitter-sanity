b79e220a0b5f62aa66b94c2d6dc48d91694af5e2
Matthew <ricklefsmatthew1003@gmail.com>
2022-05-08 14:46:34 -0400

    creates SidebarRow component, creates props interface, begins styling

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,9 +9,21 @@ import {
   UserIcon,
   HomeIcon,
 } from '@heroicons/react/outline'
+import SidebarRow from './SidebarRow'
 
 function Sidebar() {
-  return <div>I am a Sidebar</div>
+  return (
+    <div>
+      <img className="h-10 w-10" src="https://links.papareact.com/drq" alt="" />
+
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+    </div>
+  )
 }
 
 export default Sidebar
---
+++ components/SidebarRow.tsx
@@ -0,0 +1,18 @@
+import React, { SVGProps } from 'react'
+
+interface Props {
+  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
+  title: string
+}
+
+function SidebarRow({ Icon, title }: Props) {
+  return (
+    <div>
+      <Icon />
+
+      <p>{title}</p>
+    </div>
+  )
+}
+
+export default SidebarRow
