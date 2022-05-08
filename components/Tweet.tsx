b204bf5636f4a6814ace60dd72bcab38d691dd77
Matthew <ricklefsmatthew1003@gmail.com>
2022-05-08 18:10:58 -0400

    creates nested comments section within the Tweet component that reference the tweet they are associated to, introduces styling for the comments section

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -78,6 +78,40 @@ function Tweet({ tweet }: Props) {
           <UploadIcon className="h-5 w-5" />
         </div>
       </div>
+
+      {/* Comment Box Logic */}
+
+      {comments?.length > 0 && (
+        <div className="bored-t my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-gray-100 p-5">
+          {comments.map((comment) => (
+            <div key={comment._id} className=" relative flex space-x-2">
+              <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
+
+              <img
+                src={comment.profileImg}
+                className="mt-2 h-7 w-7 rounded-full object-cover"
+                alt=""
+              />
+
+              <div className="">
+                <div className="flex items-center space-x-1 ">
+                  <p className="mr-1 font-bold">{comment.username} </p>
+                  <p className="hidden text-sm text-gray-500 lg:inline">
+                    @{comment.username.replace(/\s+/g, '').toLowerCase()}.{' '}
+                  </p>
+
+                  <TimeAgo
+                    className="text-sm text-gray-500"
+                    date={comment._createdAt}
+                  />
+                </div>
+
+                <p>{comment.comment}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
